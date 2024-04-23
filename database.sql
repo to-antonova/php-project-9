21e4c7c592896e640028a470e7f4ecfcbf516329
Tatiana Antonova <to.antonova@mail.ru>
2024-04-23 15:07:21 +0700

    update database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,15 @@
-TRUNCATE urls_checks;
-TRUNCATE urls CASCADE;
+CREATE TABLE urls (
+                      id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                      name varchar(255) UNIQUE NOT NULL,
+                      created_at timestamp
+);
 
-CREATE TABLE IF NOT EXISTS urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255),
-    created_at timestamp);
-
-CREATE TABLE IF NOT EXISTS urls_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id bigint REFERENCES urls (id),
-    status_code int,
-    h1 varchar(255),
-    title varchar(255),
-    description varchar(255),
-    created_at timestamp);
+CREATE TABLE url_checks (
+                            id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                            url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+                            status_code int,
+                            h1 text,
+                            title text,
+                            description text,
+                            created_at timestamp
+);
