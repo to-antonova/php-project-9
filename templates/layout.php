d6d2afbcc9ad42663617e4371456a54b78a790cd
Tatiana Antonova <to.antonova@mail.ru>
2024-05-16 14:20:32 +0700

    lint templates

--- templates/layout.php
+++ templates/layout.php
@@ -7,7 +7,9 @@
 ?>
 
 <!DOCTYPE html>
+
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,7 +18,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
+
 <body class="min-vh-100 d-flex flex-column">
+
 <header class="flex-shrink-0">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark px-2">
         <div class="container-fluid d-flex justify-content-start px-2">
@@ -38,11 +42,15 @@
         </div>
     </nav>
 </header>
+
 <main class="flex-grow-1">
     <?= $content?>
 </main>
+
 </body>
+
 <div>
     <?= $this->fetch('footer.php') ?>
 </div>
+
 </html>
