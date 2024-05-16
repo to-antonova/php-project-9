8e6bd1809ad04f5e6758f92e68448051b33403cc
Tatiana Antonova <to.antonova@mail.ru>
2024-05-16 14:37:43 +0700

    add footer in layout

--- templates/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="border-top py-3 mt-5 flex-shrink-0">
-    <div class="container-lg">
-        <div class="text-center">
-            <a href="https://hexlet.io/pages/about" target="_blank">Hexlet</a>
-        </div>
-    </div>
-</footer>
